2ddc3fc22173d57ad9bd4d5ed1d20658e1ea0e6d
Yan Serhiienko <yanserhiienko@gmail.com>
2024-05-15 07:38:13 +0300

    init

---
+++ src/main/java/com/example/clearsolutionstestassignment/ClearSolutionsTestAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.clearsolutionstestassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClearSolutionsTestAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClearSolutionsTestAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/clearsolutionstestassignment/ClearSolutionsTestAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clearsolutionstestassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClearSolutionsTestAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
